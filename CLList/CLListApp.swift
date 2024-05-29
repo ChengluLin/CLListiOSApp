b079b5a486ab44398b7cf7acbac54cb3b6b2e846
Kevin <chenglu7701@gmail.com>
2024-05-29 11:44:15 +0800

    Created CLList project

---
+++ CLList/CLListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLListApp.swift
+//  CLList
+//
+//  Created by ChengLu on 2024/5/29.
+//
+
+import SwiftUI
+
+@main
+struct CLListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CLList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CLList
+//
+//  Created by ChengLu on 2024/5/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
